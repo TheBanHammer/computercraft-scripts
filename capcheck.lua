91cdb03c242ed11c30b70ca13b89f2c1822e527b
Mike Botsko <botsko@gmail.com>
2014-11-17 13:04:33 -0800

    Reformatting output, supports multiple reactors

--- capcheck.lua
+++ capcheck.lua
@@ -3,17 +3,22 @@
 -- Automatically checks for wired attachements for EnderIO capacitor bank, monitor, big reactor
 -- Will automatically active reactor when capacitor bank too low, or deactive when too high
 -- Connected devices take priority over modem devices
--- Requires 1x3 monitors
+-- Requires 3x3 monitors (2x3 if less than 4 reactors)
 
 -- based on code: http://pastebin.com/rjfa4ymR
 
-local version = 0.1
+local version = 0.2
 
 print("Reactor/Capacitor Manager v"..version)
  
-local upper = 0.90 --Upper limit for computer to stop transmitting redstone signal. 0.90=90% full.
-local lower = 0.10 --Lower limit for computer to start transmitting redstone signal.
+local upper = 0.95 --Upper limit for computer to stop transmitting redstone signal. 0.90=90% full.
+local lower = 0.20 --Lower limit for computer to start transmitting redstone signal.
  
+function round2(num, idp)
+  local mult = 10^(idp or 0)
+  return math.floor(num * mult + 0.5) / mult
+end
+
 --Device detection
 isError=0
  
@@ -29,7 +34,6 @@ function detectDevice(DeviceName)
 end
  
 cell = "none"
-reactor = "none"
 monitor = "none"
 local peripheralList = peripheral.getNames()
  
@@ -60,19 +64,20 @@ else
   end
 end
  
--- detect reactor
+-- detect reactors
+reactors = {}
 reactorFace=detectDevice("BigReactors-Reactor")
 if reactorFace~="none" then
-  reactor=peripheral.wrap(reactorFace)
+  table.insert(reactors,peripheral.wrap(reactorFace))
   print ("Reactor on the " .. reactorFace .. " connected.")
 else
   for Index = 1, #peripheralList do
     if string.find(peripheralList[Index], "Reactor") then
-      reactor=peripheral.wrap(peripheralList[Index])
+      table.insert(reactors,peripheral.wrap(peripheralList[Index]))
       print ("Reactor on wired modem: "..peripheralList[Index].." connected.")
     end
   end
-  if reactor == "none" then
+  if table.getn(reactors) == 0 then
     print("No reactors found. Halting script!")
     return
   end
@@ -91,24 +96,37 @@ else
     end
   end
   if monitor == "none" then
-    print ("Warning - No Monitor attached, continuing without.")
+    print("No monitor found. Halting script!")
+    return
   end
 end
 
 -- default to no redstone signal
 redstone.setOutput("back", false)
- 
---If monitor is attached, write data on monitor
-if monitor ~= "none" then
-  monitor.clear()
-  monitor.setBackgroundColour((colours.grey))
-  monitor.setCursorPos(1,4)
-  monitor.write(" ON ")
-  monitor.setBackgroundColour((colours.red))
-  monitor.setCursorPos(5,4)
-  monitor.write(" OFF ")
-  monitor.setBackgroundColour((colours.black))
+
+function setReactorState( state )
+  for i, reactor in ipairs(reactors) do
+    reactor.setActive(state)
+  end
 end
+
+-- turn all reactors off
+setReactorState(false)
+ 
+-- --If monitor is attached, write data on monitor
+monitor.clear()
+
+monitor.setCursorPos(1,6)
+monitor.write("Reactors")
+
+monitor.setCursorPos(10,8)
+monitor.write("Buff")
+
+monitor.setCursorPos(16,8)
+monitor.write("Temp")
+
+monitor.setCursorPos(22,8)
+monitor.write("Output")
 
 eLast = 0;
  
@@ -118,98 +136,106 @@ while true do
   eNow = cell.getEnergyStored("unknown")
   eMax = cell.getMaxEnergyStored("unknown")
   fill = (eNow / eMax)
-  perc = math.floor(fill*100)
+  perc = fill*100
   diff = eNow - eLast
   eLast = eNow;
  
-  if monitor ~= "none" then
+  displayStorage = math.ceil(eNow)
+  displayCapacity = math.ceil(eMax)
+  displayFill = round2(perc,2).."%"
+  displayChange = round2(diff,2)
 
-    if eMax >= 10000000 then
-      monitor.setCursorPos(11,1)
-      monitor.write("Storage:")
-      monitor.setCursorPos(11,2)
-      monitor.setBackgroundColour((colours.blue))
-      monitor.write(math.ceil(eNow/1000).."kRF")
-      monitor.setBackgroundColour((colours.black))
-      monitor.setCursorPos(11,3)
-      monitor.write("Capacity:")
-      monitor.setCursorPos(11,4)
-      monitor.setBackgroundColour((colours.blue))
-      monitor.write(math.ceil(eMax/1000).."kRF")
-      monitor.setBackgroundColour((colours.black))
-      monitor.setCursorPos(21,1)
-      monitor.write("Fill:")
-      monitor.setCursorPos(21,2)
-      monitor.setBackgroundColour((colours.cyan))
-      monitor.write(perc.."%")
-      monitor.setBackgroundColour((colours.black))
-      monitor.setCursorPos(21,4)
-      monitor.write("Change:")
-      monitor.setCursorPos(21,5)
-      monitor.clearLine()
-      monitor.setBackgroundColour((colours.cyan))
-      monitor.write(math.ceil(diff/1000).."k/s")
-      monitor.setBackgroundColour((colours.black))
-    else   
-      monitor.setCursorPos(11,1)
-      monitor.write("Storage:")
-      monitor.setCursorPos(11,2)
-      monitor.setBackgroundColour((colours.blue))
-      monitor.write(math.ceil(eNow))
-      monitor.setBackgroundColour((colours.black))
-      monitor.setCursorPos(11,3)
-      monitor.write("Capacity:")
-      monitor.setCursorPos(11,4)
-      monitor.setBackgroundColour((colours.blue))
-      monitor.write(math.ceil(eMax))
-      monitor.setBackgroundColour((colours.black))
-      monitor.setCursorPos(21,1)
-      monitor.write("Fill:")
-      monitor.setCursorPos(21,2)
-      monitor.setBackgroundColour((colours.cyan))
-      monitor.write(perc.."%")
-      monitor.setBackgroundColour((colours.black))
-      monitor.setCursorPos(21,4)
-      monitor.write("Change:")
-      monitor.setCursorPos(21,5)
-      monitor.clearLine()
-      monitor.setBackgroundColour((colours.cyan))
-      monitor.write(math.ceil(diff))
-      monitor.setBackgroundColour((colours.black))
-    end
-    monitor.setCursorPos(1,2)
-    monitor.write("Reactors:")
+  -- over a billion
+  if eMax >= 1000000000 then
+    displayCapacity = math.ceil(eMax/1000000000).."bRF"
   end
+
+  -- over a million
+  if eMax >= 1000000 then
+    displayStorage = math.ceil(eNow/1000).."kRF"
+    displayCapacity = math.ceil(eMax/1000).."kRF"
+    displayChange = round2(diff/1000,2).."k"
+  end
+
+  monitor.setCursorPos(1,1)
+  monitor.write("Storage:")
+  monitor.setCursorPos(1,2)
+  monitor.clearLine()
+  monitor.setBackgroundColour((colours.blue))
+  monitor.write(displayStorage)
+  monitor.setBackgroundColour((colours.black))
+  monitor.setCursorPos(1,3)
+  monitor.write("Capacity:")
+  monitor.setCursorPos(1,4)
+  monitor.clearLine()
+  monitor.setBackgroundColour((colours.blue))
+  monitor.write(displayCapacity)
+  monitor.setBackgroundColour((colours.black))
+  monitor.setCursorPos(16,1)
+  monitor.write("Fill:")
+  monitor.setCursorPos(16,2)
+  monitor.setBackgroundColour((colours.cyan))
+  monitor.write(displayFill)
+  monitor.setBackgroundColour((colours.black))
+  monitor.setCursorPos(16,3)
+  monitor.write("Change:")
+  monitor.setCursorPos(16,4)
+  monitor.setBackgroundColour((colours.cyan))
+  monitor.write(displayChange)
+  monitor.setBackgroundColour((colours.black))
    
   if fill > upper then
     redstone.setOutput("back", false)
-    reactor.setActive(false)
-    print("Deactivating reactors...")
-   
-    if monitor ~= "none" then
-      monitor.setBackgroundColour((colours.grey))
-      monitor.setCursorPos(1,4)
-      monitor.write(" ON ")
-      monitor.setBackgroundColour((colours.red))
-      monitor.setCursorPos(5,4)
-      monitor.write(" OFF ")
-      monitor.setBackgroundColour((colours.black))
-    end
+    setReactorState(false)
   elseif fill < lower then
-
     redstone.setOutput("back", true)
-    reactor.setActive(true)
-    print("Activating reactors...")
-   
-    if monitor ~= "none" then
+    setReactorState(true)
+  end
+
+  cursorY = 9
+
+  -- reactor status
+  for i, reactor in ipairs(reactors) do
+    monitor.setCursorPos(1,cursorY)
+    monitor.clearLine()
+    monitor.write(i..":")
+    monitor.setCursorPos(4,cursorY)
+
+    if reactor.getActive() then
       monitor.setBackgroundColour((colours.green))
-      monitor.setCursorPos(1,4)
       monitor.write(" ON ")
-      monitor.setBackgroundColour((colours.grey))
-      monitor.setCursorPos(5,4)
+      monitor.setBackgroundColour((colours.black))
+    else
+      monitor.setBackgroundColour((colours.red))
       monitor.write(" OFF ")
       monitor.setBackgroundColour((colours.black))
     end
+
+    -- internal buffer
+    displayEnergyBuffer = math.floor((reactor.getEnergyStored()/10000000)*100).."%"
+    monitor.setCursorPos(10,cursorY)
+    monitor.write(displayEnergyBuffer)
+
+    -- temp
+    displayTemperature = math.ceil(reactor.getCasingTemperature()).."c"
+    if reactor.getCasingTemperature() > 1000 then
+      displayTemperature = round2(reactor.getCasingTemperature()/1000,2).."k c"
+    end
+
+    monitor.setCursorPos(16,cursorY)
+    monitor.write(displayTemperature)
+
+    -- energy produced
+    displayEnergyProduced = round2(reactor.getEnergyProducedLastTick(),2).."RF"
+    if reactor.getEnergyProducedLastTick() > 1000 then
+      displayEnergyProduced = round2(reactor.getEnergyProducedLastTick()/1000,2).."kRF"
+    end
+    monitor.setCursorPos(22,cursorY)
+    monitor.write(displayEnergyProduced)
+
+    
+
+    cursorY = cursorY + 1
   end
     
   sleep(1)
